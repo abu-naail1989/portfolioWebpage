16acf17331b866a6eb272c38dd9bf21db90bf33c
James Alhassan <jamesalhassan052@gmail.com>
2022-07-20 08:48:25 +0100

    fixed header

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ btn.addEventListener("click", function () {
     btn.classList.remove("slide");
   }
 });
+
+const header = document.querySelector("#header");
+
+window.addEventListener("scroll", function () {
+  const headerHeight = header.getBoundingClientRect().height;
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > headerHeight) {
+    header.classList.add("fixed-header");
+  } else {
+    header.classList.remove("fixed-header");
+  }
+});
